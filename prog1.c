cd473b0ba7ea55da52305449a71382abec80428e
Prashantelagi <prashant.elagi123@gmail.com>
2024-11-04 15:15:56 +0530

    Update prog1.c

--- prog1.c
+++ prog1.c
@@ -3,6 +3,7 @@ void main()
 {
 	print("hello world");
 	printf("Enter the World");
+	printf("Enter the Prashant");
 hello world
  i have added this line  in remotrepo on saturday
 this line i m adding in mylocal repotory on monday
